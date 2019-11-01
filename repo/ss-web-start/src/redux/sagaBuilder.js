af89446d8ce9a37539d1bff726cb614d6ce4b942
keeping <starunaway@gmail.com>
2019-11-01 09:52:18 +0800

    update

--- repo/ss-web-start/src/redux/sagaBuilder.js
+++ repo/ss-web-start/src/redux/sagaBuilder.js
@@ -48,3 +48,60 @@ function getEffect(item) {
     return yield fetch(item.url(payload), option);
   };
 }
+
+export function createSaga(item, {onEffect, onFetchOption, history}) {
+  const action = item.action || item.key;
+  return function*() {
+    let take = item.takeEvery || effects.takeEvery;
+    yield take(action, function*(actions) {
+      let response;
+      const effect = item.effect || getEffect(item);
+      let putAction = {
+        type: action,
+        payload: actions.payload
+      };
+      try {
+        let type = item.type || 'json';
+        let bodyParser = item.body || bodyHandler;
+        let option = createOptions(item.method, type, item.headers, bodyParser(actions.payload, type, item.method));
+        if (typeof onFetchOption === 'function') {
+          option = onFetchOption(option, item);
+        }
+        response = yield effect(actions, {fetch: Fetch.fetch, options}, {...effects}, item);
+      } catch (error) {
+        putAction.success = false;
+        putAction.loading = false;
+        putAction.result = null;
+        putAction.message = error || '请求异常';
+        putAction.type = `${putAction.type}_FAIL`;
+        yield effects.put(putAction);
+      }
+
+      if (response) {
+        if (typeof onEffect === 'function') {
+          putAction.url = item.url(actions.payload);
+          putAction = yield onEffect(putAction, response, effects, history);
+        } else {
+          putAction.loading = false;
+          putAction.success = response.status === 200;
+          putAction.status = response.status;
+          putAction.result = yield response.json();
+        }
+        putAction.type = `${putAction.type}${putAction.success ? '_SUCCESS' : '_FAIL'}`;
+        yield effects.put(putAction);
+      }
+    });
+  };
+}
+
+function createOptions(method, type, extHeaders, payload) {
+  let options = {method};
+  options.headers = extHeaders || {
+    Accept: 'application/json',
+    'Content-Type': type === 'json' ? 'application/json' : 'application/x-www-form-urlencoded'
+  };
+  if (method !== 'get') {
+    options.body = payload;
+  }
+  return options;
+}
