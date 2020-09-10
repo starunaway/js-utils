a35b4aaa7451204de14ec35fd273f75beadcb4fe
keeping <starunaway@gmail.com>
2020-09-10 09:58:19 +0800

    feat: 🎸 deepClone

--- funcs/clone.js
+++ funcs/clone.js
@@ -9,13 +9,37 @@ function shallowClone(obj) {
     return [...obj];
   } else if (/^date|error|regexp|map|set|weakmap|waeakset$/i.test(getType(obj))) {
     return new Ctor(obj);
+  } else if (/^symbol|bigint$/i.test(getType(obj))) {
+    return Object(obj);
+  } else if (/^function$/i.test(getType(obj))) {
+    return function () {
+      return obj.call(this, ...arguments);
+    };
   } else {
     return obj;
   }
 }
 
-function deepClone(obj) {
-  return shallowClone(obj);
+function deepClone(obj, cache = new Set()) {
+  if (!/^object|array$/i.test(getType(obj))) {
+    return shallowClone(obj);
+  }
+
+  // 避免无限套娃  -> 对象的所层级下引用了同一个对象
+  if (cache.has(obj)) {
+    return obj;
+  }
+  cache.add(obj);
+  let Ctor = obj.constructor;
+
+  let keys = [...Object.keys(obj), ...Object.getOwnPropertySymbols(obj)];
+  let result = new Ctor();
+
+  keys.forEach((key) => {
+    result[key] = deepClone(obj[key], cache);
+  });
+
+  return result;
 }
 
 exports.shallowClone = shallowClone;
